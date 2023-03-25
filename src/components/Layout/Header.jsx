6efc3756df2f072448a8f2b3903a00f48ad55489
Clara Coatanoan <ccoatanoan@gmail.com>
2023-03-25 18:43:04 +0100

    import React from react

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -6,11 +6,20 @@ function Header() {
     return(
         <header className='header'>
             <div className='header_logo'>   
-                <img src='{logo}' alt='logo-kasaa' className='header_logo_img' />
+                <img src={logo} alt='Logo Kasa' className='header_logo_img' />
             </div>
             <nav className='header_nav'>
-                
+                <ul>
+                    <li className='header_nav_link'> 
+                        <NavLink>
+
+                        </NavLink>
+                    </li>
+                </ul>
+               
             </nav>
         </header>
     )
 }
+
+export default Header
