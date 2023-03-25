0a584d2ee74c35c26265f3f11905b1fc8d771851
Clara Coatanoan <ccoatanoan@gmail.com>
2023-03-25 18:47:08 +0100

    add comment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Layout/Header'
+//importer toutes les layout de la page home (footer, banner, allcards)
 
 function Home() {
     return(
