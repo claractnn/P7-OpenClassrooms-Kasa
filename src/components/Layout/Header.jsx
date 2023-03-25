90160480132f685f1944b495459b2a482c111053
Clara Coatanoan <ccoatanoan@gmail.com>
2023-03-25 18:38:47 +0100

    move files scss to folder layout

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import logo from '../../styles/images/';
+import logo from '../../styles/images/logo_kasa.png';
 
 function Header() {
     return(
-
+        <header className='header'>
+            <div className='header_logo'>   
+                <img src='{logo}' alt='logo-kasaa' className='header_logo_img' />
+            </div>
+            <nav className='header_nav'>
+                
+            </nav>
+        </header>
     )
 }
