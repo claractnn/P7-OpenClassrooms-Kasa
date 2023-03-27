e04f46ea6a2f5741dd0cecaa5d59b47e7bb69161
Clara Coatanoan <ccoatanoan@gmail.com>
2023-03-27 15:27:34 +0200

    add navlink home and about

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -11,7 +11,20 @@ function Header() {
             <nav className='header_nav'>
                 <ul>
                     <li className='header_nav_link'> 
-
+                        <NavLink 
+                            className={({isActive}) => (isActive ? 'header_nav_link_navlink header_nav_link_active' : 'header_nav_link_navlink header_nav_link_inactive' )}
+                            title='Accueil'
+                            to='/home'>
+                                Accueil
+                        </NavLink>
+                    </li>
+                    <li className='header_nav_link'> 
+                        <NavLink 
+                            className={({isActive}) => (isActive ? 'header_nav_link_navlink header_nav_link_active' : 'header_nav_link_navlink header_nav_link_inactive' )}
+                            title='À propos'
+                            to='/about'>
+                                À propos
+                        </NavLink>
                     </li>
                 </ul>
                
