d1352f8be157ada4ae34571050fb7262252550ce
Clara Coatanoan <ccoatanoan@gmail.com>
2023-03-30 15:35:59 +0200

    add function

--- src/components/Layout/Banner.jsx
+++ src/components/Layout/Banner.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import img_banner from '../../styles/images/img_banner.webp'
 
-function Banner() {
+function Banner(props) {
     return (
         <div className="banner">
-            
+            <h1 className='banner_title'>{props.title}</h1>
+            <img src={props.src} alt='Bannière Kasa' className='banner_img' />
         </div>
     )
 }
 
-export default Banner
+export default Banner;
