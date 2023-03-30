0055ec2069eea309499ff530835ca51212279e98
Clara Coatanoan <ccoatanoan@gmail.com>
2023-03-30 16:02:05 +0200

    add name function

--- src/components/About/AboutBanner.jsx
+++ src/components/About/AboutBanner.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import imgBanner from '../../styles/images/img_about_banner.webp';
 import Banner from '../Layout/Banner';
 
-function HomeBanner() {
+function AboutBanner() {
     return (
         <Banner 
         src={imgBanner}
         />
     )
 }
-export default HomeBanner
+export default AboutBanner
