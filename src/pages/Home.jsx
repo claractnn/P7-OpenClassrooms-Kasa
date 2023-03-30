971f53b7c5fd8b390f1de3112c3bbebeae086226
Clara Coatanoan <ccoatanoan@gmail.com>
2023-03-30 15:37:15 +0200

    import file  home banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../components/Layout/Header'
-import HomeBanner from '../components/Layout/Banner'
+import HomeBanner from '../components/Home/HomeBanner'
 //importer toutes les layout de la page home (footer, banner, allcards)
 
 function Home() {
