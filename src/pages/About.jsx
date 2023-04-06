b2d8e75246d725c1dfdf0f2f31285ea30eaa2f85
Clara Coatanoan <ccoatanoan@gmail.com>
2023-04-06 15:56:38 +0200

    remove comment

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,6 @@ import AboutBanner from '../components/About/AboutBanner';
 import Scrollings from '../components/About/Scrollings';
 import Values from '../datas/values.json';
 import Footer from '../components/Layout/Footer';
-//importer toutes les layout de la page home (footer, banner, allcards)
 
 function About() {
     return(
