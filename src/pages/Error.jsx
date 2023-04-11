02f2c646c2131356b809998b81d27be16bb1e009
Clara Coatanoan <ccoatanoan@gmail.com>
2023-04-11 12:12:58 +0200

    added style all page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Header from '../components/Layout/Header';
+import Footer from '../components/Layout/Footer';
 
 function Error() {
     return ( 
-        <section className='error'>
-            <div className='error_title'>
-                <h2 className='error_title_number'>404</h2>
-                <p className='error_title_message'>Oups ! La page que vous demandez n'existe pas.</p>
-            </div>
-            <div>
-                <p className='error_button'>Retourner sur la page d'accueil</p>
-            </div>
-
-        </section>
+        <div>
+            <Header />
+            <section className='error'>
+                <div className='error_title'>
+                    <h1 className='error_title_number'>404</h1>
+                    <p className='error_title_message'>Oups ! La page que vous demandez n'existe pas.</p>
+                </div>
+                    <Link to='/'>Retourner sur la page d'accueil</Link>               
+            </section>
+            <Footer />
+        </div>
     )
 }
 
