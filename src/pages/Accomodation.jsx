91f515976fe85433cb4e434aa4718b6adcd3da0a
Clara Coatanoan <ccoatanoan@gmail.com>
2023-04-12 09:17:39 +0200

    added first import

---
+++ src/pages/Accomodation.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Navigate, useParams } from 'react-router-dom';
+import Header from '../components/Layout/Header';
+import Footer from '../components/Layout/Footer';
+
+/*function Accomodation() {
+    return (
+
+    )
+}*/
