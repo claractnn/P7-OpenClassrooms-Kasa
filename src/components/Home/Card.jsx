e5f07b85e7a67fcce85e7b63e2423ff84600d97a
Clara Coatanoan <ccoatanoan@gmail.com>
2023-04-12 09:17:46 +0200

    added first import

--- src/components/Home/Card.jsx
+++ src/components/Home/Card.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Card(props) {
     const { id , title, cover } = props.accomodation 
