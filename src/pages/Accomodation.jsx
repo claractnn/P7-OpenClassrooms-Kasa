99f40e286493ac6d9f56f2d6f5689b776dc852dd
Clara Coatanoan <ccoatanoan@gmail.com>
2023-04-13 10:57:07 +0200

    modify arrow react icons

--- src/pages/Accomodation.jsx
+++ src/pages/Accomodation.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-//import { Navigate, useParams } from 'react-router-dom';
+import { Navigate, useParams } from 'react-router-dom';
 import Header from '../components/Layout/Header';
 import Footer from '../components/Layout/Footer';
 import Carrousel from '../components/Accomodation/Carrousel';
+import accomodations from '../datas/accomodations.json';
 
 function Accomodation() {
-    return (
+    const { id } = useParams();
+    const accomodation = accomodations.find((house) => house.id === id)
+
+    return !accomodation ? (<Navigate to="/error" />) : ( 
         <div>
             <Header />
+            <section className='accomodation'>
+                
+            </section>
             <Carrousel />
             <Footer />
         </div>
