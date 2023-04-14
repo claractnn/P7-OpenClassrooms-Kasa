998dc948b69f9da647f712f4ad124195c0ea0ecf
Clara Coatanoan <ccoatanoan@gmail.com>
2023-04-14 16:07:19 +0200

    add class

--- src/components/Accomodation/Tag.jsx
+++ src/components/Accomodation/Tag.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Tag() {
-    return(
-
-    )
-}
+class Tag extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        const { tag } = this.props;
+        return(
+            <div className='tag'>
+                <h3>{tag}</h3>
+            </div>
+            )
+        }
+    }
+  
 
 export default Tag;
