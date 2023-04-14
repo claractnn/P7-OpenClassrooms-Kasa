90e83888c619ab4ac500f9b9bafee425e3dcac8f
Clara Coatanoan <ccoatanoan@gmail.com>
2023-04-14 16:04:54 +0200

    added Tag

--- src/components/Accomodation/Presentation.jsx
+++ src/components/Accomodation/Presentation.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
-function Presentation() {
+function Presentation({ title, location, tags, ratin, host }) {
     return (
-        <div
-        ></div>
+        <section className='presentation'>
+            <div>
+                <h1>{title}</h1>
+                <h2>{location}</h2>
+                <div className='tags'>
+                    {tags.map((tag, index) => {
+                        return <Tag key={index} tag={tag} />;
+                    })}
+                </div>
+            </div>
+
+        </section>
     )
 }
 
